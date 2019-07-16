8d24698f62f0430bb908b10eb4d28580568be960
Julio Fuentes <juliofuentes408@gmail.com>
2019-07-16 16:58:33 -0700

    Implement relative proxy server routes to appropriate API's

--- proxy/server/server.js
+++ proxy/server/server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const httpProxy = require('http-proxy'); 
+const apiProxy = httpProxy.createProxyServer();
+const bookingsUrl = "http://localhost:3000",
+      relatedListingsUrl = "http://localhost:3001",
+      galleryContainerUrl = "http://localhost:1128";
 
 const app = express();
 
@@ -10,6 +15,30 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time MS'))
 app.use(express.static(__dirname + '/../client/dist'));
 
 app.use(bodyParser());
+
+app.get('/rooms/bookings/listings', (req,res) => {
+    apiProxy.web(req,res, {target: bookingsUrl});
+});
+
+app.get('/rooms/bookings/dates/:month', (req, res) => {
+    apiProxy.web(req,res,{target: bookingsUrl});
+});
+
+app.get('/rooms/bookings/listing_dates', (req,res) => {
+    apiProxy.web(req,res,{target: bookingsUrl});
+});
+
+
+//BELOW IS RELATED LISTINGS
+app.get('/rooms/related-listings', (req,res) => {
+    apiProxy.web(req,res,({target: relatedListingsUrl}));
+});
+
+//BELOW IS GALLERY CONTAINER MODULE
+app.get('/images', (req,res) => {
+    apiProxy.web(req,res,({target: galleryContainerUrl}));
+});
+
 
 const PORT = 1111;
 
